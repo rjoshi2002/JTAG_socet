00637ec4729df2b31445e2933907e828751e5a96
Yunlin0715 <89328063+Yunlin0715@users.noreply.github.com>
2024-03-20 15:37:12 -0400

    Add files via upload

--- src/ahb_ap.sv
+++ src/ahb_ap.sv
@@ -13,30 +13,28 @@ module ahb_ap (
     typedef enum logic [2:0] { 
         IDLE = 3'b0, 
         ADDR = 3'd1,
-        BUF = 3'd2, 
+        READ = 3'd2, 
         WAIT = 3'd3, 
         DATA = 3'd4, 
-        WRITE_WAIT = 3'd5 
+        WRITE = 3'd5 
     } state_t;
 
     state_t state, next_state; 
-
-    logic [41:0] ahb_data;
-
+    logic [40:0] ahb_data;
     logic [31:0] real_data;  
-
     logic [1:0] size_ahb; 
+    logic [4:0] addr_inc;
+    logic [5:0] count, next_count;  
 
-    logic [4:0] addr_inc; 
+    // decode instruction 
+    assign real_data = apif.rdata_fifo1[41:10]; 
+    assign reg_select = apif.rdata_fifo1[9]; 
+    assign size_ahb = apif.rdata_fifo1[8:7]; 
+    assign addr_inc = apif.rdata_fifo1[6:2];
+    assign r_or_w = apif.rdata_fifo1[1]; // read 0, write 1 
+    
 
-    assign real_data = apif.data[41:10]; 
-    assign reg_select = apif.data[9]; 
-    assign size_ahb = apif.data[8:7]; 
-    assign addr_inc = apif.data[6:2];
-    assign r_or_w = apif.data[1]; // read 0, write 1 
-    assign empty = apif.data[0]; 
-
-    always_ff @( AFT_CLK, TRST ) begin 
+    always_ff @( posedge AFT_CLK, negedge TRST ) begin 
         if(1'b0 == TRST) begin
             state <= IDLE; 
         end else begin
@@ -44,24 +42,65 @@ module ahb_ap (
         end
     end
 
+    always_comb begin: byte_en_logic 
+        if(size_ahb == 2'b00) begin
+            byte_en = 4'd0; // byte
+        end else if (size_ahb == 2'd1 || size_ahb == 2'd2) begin
+            byte_en = 4'b1100; // half word 
+        end else begin
+            byte_en = 4'b1111; // word 
+        end 
+    end 
+
     always_comb begin : state_transition
         next_state = state; 
         casez (state)
             IDLE: begin
-                if(apif.en & !reg_select) begin
+                if(!apif.rempty & reg_select) begin
                     next_state = ADDR; 
-                end else if (apif.en & reg_select) begin
+                end else if (!apif.rempty & reg_select & !addr_inc) begin
                     next_state = DATA; 
                 end else begin
                     next_state = IDLE; 
                 end
             end
-            ADDR: next_state = busy ? ADDR : BUF; 
-            BUF: next_state = r_or_w ? WAIT : IDLE; 
-            DATA: next_state = r_or_w ? IDLE : WRITE_WAIT; 
-            WRITE_WAIT: next_state = busy ? WRITE_WAIT : IDLE; 
-            default: 
+            ADDR: next_state = r_or_w ? WAIT : READ; 
+            READ: next_state = (apif.busy & (count == addr_inc)) ? IDLE : READ; 
+            WAIT: next_state = apif.rempty ? WAIT: DATA;
+            DATA: next_state = (r_or_w) ? WRITE : IDLE; 
+            WRITE: next_state = apif.busy ? IDLE : WRITE; 
         endcase
+    end
+
+    always_comb begin : output_logic
+        apif.ren = 0; 
+        apif.wen = 0; 
+        apif.byte_en = '0; 
+        apif.rinc = 0; 
+        apif.winc = 0; 
+        apif.wdata_fifo2 = '0; 
+        apif.wdata_aft = '0; 
+        apif.addr_aft = '0; 
+
+        casez (state)
+            ADDR: 
+        endcase
+
+
+    end
+
+    // counter 
+    always_ff @( posedge AFT_CLK, negedge TRST ) begin : counter
+        if(!TRST) begin
+            count <= '0; 
+        end else begin
+            count <= next_count; 
+        end 
+    end
+
+    always_comb begin : count_logic 
+        if(count == 40) begin
+
     end
 
 
