5b1b4e0808466d08ef846935e3bf33ed223a9525
Yunlin0715 <89328063+Yunlin0715@users.noreply.github.com>
2024-04-10 14:52:02 -0400

    Add files via upload

---
+++ src/ahb_ap_generic_bus.sv
@@ -0,0 +1,12 @@
+/* Linda Zhang
+    AHB access point with connection to generic bus design 
+*/ 
+
+`include "ahb_ap_if.vh"
+`include "jtag_types_pkg.vh"
+
+module ahb_ap_generic_bus (
+    
+);
+    
+endmodule
